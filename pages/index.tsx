a84e8818993489325b64abb3f8302dba599b3294
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2023-05-21 15:43:56 +0400

    fix routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
+
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "../hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const Home: NextPage = () => {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1>Netflix Clone</h1>
+      <h1 className="text-4xl text-red-200">Netflix Clone</h1>
+      <p className="text-white"> Logged in as : {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        LogOut
+      </button>
     </>
   );
 };
