b5909a6444534c6540736a90b9b416c843872a95
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2023-05-21 18:05:40 +0400

    hero video

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage, NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 import useCurrentUser from "../hooks/useCurrentUser";
 import Navbar from "../components/Navbar";
+import Billboard from "../components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -27,6 +28,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 };
